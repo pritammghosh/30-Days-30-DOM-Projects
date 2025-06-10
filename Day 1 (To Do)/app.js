26025d1fd08097e11e24feab7290bab7371d3bb4
pritammghosh <pritammmghosh@gmail.com>
2025-06-10 14:38:39 +0530

    Added Day 1 - To Do Project

---
+++ Day 1 (To Do)/app.js
@@ -0,0 +1,43 @@
+const input = document.getElementById("taskInput");
+const addBtn = document.getElementById("addBtn");
+const taskList = document.getElementById("taskList");
+
+addBtn.addEventListener("click", () => {
+  const task = input.value.trim();
+  if (task === "") {
+    alert("Enter a task!");
+    return;
+  }
+
+  // Create list item
+  const li = document.createElement("li");
+
+  // Inserting the Text and Button
+  li.innerHTML = `
+    <span class="taskText">${task}</span>
+    <span>
+      <button class="editBtn">Edit</button>
+      <button class="deleteBtn">Delete</button>
+    </span>
+  `;
+
+  // Append to task and recent added to the Top 
+  taskList.prepend(li);
+  input.value = "";
+
+  // Function Added to Delete the Task
+  const deleteBtn = li.querySelector(".deleteBtn");
+  deleteBtn.addEventListener("click", () => {
+      li.remove();
+  });
+
+  // Function Added to Edit the Task
+  const editBtn = li.querySelector(".editBtn");
+  editBtn.addEventListener("click", () => {
+    const taskSpan = li.querySelector(".taskText");
+    const newTask = prompt("Edit your task", taskSpan.textContent.trim());
+    if (newTask) {
+      taskSpan.textContent = newTask;
+    }
+  });
+});
